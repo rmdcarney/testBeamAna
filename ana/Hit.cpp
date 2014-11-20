0c9706772c59ec071de28ac28da74360da780f06
carduelia <rmdcarney@gmail.com>
2014-11-20 14:57:12 -0800

    HitContainer redefined to be a vector of hits

---
+++ ana/Hit.cpp
@@ -0,0 +1,64 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//Project:	High Eta TB Analysis
+//Author:	RMD Carney
+//Contact:	rcarney@lbl.gov
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//File:		Hit.cpp
+//Description:	Contains hit info
+//Date: 	11/14
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#include <Hit.h>
+#define DEBUG 1
+
+Hit::Hit() {
+	bcid=-1;
+	col=0;
+	row=0;
+	tot=-1;
+
+}
+
+Hit::Hit(int arg_bcid, int arg_col, int arg_row, int arg_tot){
+	bcid = arg_bcid;
+	col = arg_col;
+	row = arg_row;
+	tot = arg_tot;
+}
+
+//Getters
+
+int Hit::get_bcid(){
+	return bcid;
+}
+
+int Hit::get_col(){
+	return col;
+}
+
+int Hit::get_row(){
+	return row;
+}
+
+int Hit::get_tot(){
+	return tot;
+}
+
+//Setters
+
+void Hit::set_bcid(int arg_bcid){
+	bcid = arg_bcid;
+}
+
+void Hit::set_col(int arg_col){
+	col = arg_col;
+}
+
+void Hit::set_row(int arg_row){
+	row = arg_row;
+}
+
+void Hit::set_tot(int arg_tot){
+	tot = arg_tot;
+}
+
---
+++ ana/Hit.h
@@ -0,0 +1,61 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//Project:	High Eta TB Analysis
+//Author:	RMD Carney
+//Contact:	rcarney@lbl.gov
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//File:		Hit.h
+//Description:	Contains hit info
+//Date: 	11/14
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#ifndef HIT_H
+#define HIT_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <boost/lexical_cast.hpp>
+#include <sstream>
+
+using namespace std;
+
+class Hit{
+	public:
+		//Constructors, destructors
+		Hit();
+		Hit(int arg_bcid, int arg_col, int arg_row, int arg_tot);
+		~Hit(){};
+
+		//Methods
+		
+		//Getters
+		//TODO vector<Hit> getHits();
+		int get_bcid();
+		int get_col();
+		int get_row();
+		int get_tot();
+
+		//******* Setters ********
+		//TODO void setData(std::vector<double> newT, std::vector<double> newY);
+		void set_bcid(int arg_bcid);
+		void set_col(int arg_col);
+		void set_row(int arg_row);
+		void set_tot(int arg_tot);
+
+
+	private:
+		int bcid;
+		int col;
+		int row;
+		int tot;
+
+		//TODO std::vector<Hit> hits;
+		
+};
+
+typedef std::vector<Hit> Hits;
+#endif
+
+
