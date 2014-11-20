a72a3cfd7b6dd2ac085e565a39532285f7424cb1
carduelia <rmdcarney@gmail.com>
2014-11-20 14:56:41 -0800

    Changed what an EventContainer means: a vector of events. Events are now mapped with an link key

---
+++ ana/Event.cpp
@@ -0,0 +1,73 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//Project:	High Eta TB Analysis
+//Author:	RMD Carney
+//Contact:	rcarney@lbl.gov
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//File:		Event.cpp
+//Description:	Contains hits & timing
+//Date: 	11/14
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#include <Event.h>
+#define DEBUG 1
+
+Event::Event() {
+	lv1id=0;
+	//Hit container - ready!
+	Hits hits;
+}
+
+Event::Event(unsigned arg_lv1id){
+	lv1id = arg_lv1id;
+	//Hit container - ready!
+	Hits hits;
+}
+
+
+void Event::addHit(Hit arg_hit){
+	hits.push_back(arg_hit);
+}
+
+void Event::addHit(unsigned bcid, unsigned col, unsigned row, unsigned tot){
+	hits.push_back(Hit(bcid, col, row, tot));
+}
+
+//Getters
+
+std::vector<Hit> Event::getHits(){
+	return hits;
+}
+
+
+int Event::get_nHits(){
+	return hits.size();
+}
+
+int Event::get_lv1id(){
+	return lv1id;
+}
+
+int Event::get_link(){
+	return link;
+}
+
+int Event::get_chipID(){
+	return chipID;
+}
+
+
+//Setters
+
+void Event::set_lv1id(unsigned arg_lv1id){
+	lv1id = arg_lv1id;
+}
+
+void Event::set_chipID(unsigned arg_chipID){
+	chipID = arg_chipID;
+}
+
+void Event::set_link(unsigned arg_link){
+	link = arg_link;
+}
+
+
---
+++ ana/Event.h
@@ -0,0 +1,64 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//Project:	High Eta TB Analysis
+//Author:	RMD Carney
+//Contact:	rcarney@lbl.gov
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//File:		Event.h
+//Description:	Contains hits & timing
+//Date: 	11/14
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <boost/lexical_cast.hpp>
+#include <sstream>
+
+#include "Hit.h"
+
+using namespace std;
+
+class Event{
+	public:
+		//Constructors, destructors
+		Event();
+		Event(unsigned arg_lv1id);
+		~Event(){};
+
+		//Methods
+		void addHit(Hit arg_hits);
+		void addHit(unsigned bcid, unsigned col, unsigned row, unsigned tot);
+
+		//Getters
+		vector<Hit> getHits();
+		int get_nHits();
+		int get_lv1id();
+		int get_chipID();
+		int get_link();
+
+
+		//******* Setters ********
+		//TODO void setData(std::vector<double> newT, std::vector<double> newY);
+		void set_lv1id(unsigned arg_lv1id);
+		void set_chipID(unsigned arg_chipID);
+		void set_link(unsigned arg_link);
+
+	private:
+		unsigned lv1id;
+		unsigned chipID;
+		unsigned link;
+		std::vector<Hit> hits;
+		
+};
+
+typedef std::vector<Event> EventContainer;
+typedef std::map<unsigned,EventContainer*> EventMap;
+
+#endif
+
+
