d024aa7a90b949898696c88bf134ec59dabc31a5
carduelia <rmdcarney@gmail.com>
2014-11-23 12:20:02 -0800

    stuff seg faults

--- src/main.cxx
+++ src/main.cxx
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]){
 	}
 	std::string filename = argv[1];
 	//Try reading the data
+	
 	RCEDataReader reader(filename);
 
 	EventMap events;
@@ -39,4 +40,46 @@ int main(int argc, char* argv[]){
 		<< "\n====================" <<std::endl;
 	std::cout <<"\nOut of " <<  numEvents << " events." <<std::endl; 
 
+	//Try and access some hits and print them
+	std::list<Hit>::iterator hitIt;
+	unsigned bcid=100000000;
+	unsigned sizeOfHits = 10000000;
+	//for(hitIt = events[0]->at(0).getHits().begin(); hitIt != events[0]->at(0).getHits().end(); ++hitIt){
+	
+	sizeOfHits = events[0]->at(0).get_nHits();
+
+	//Iterate over link 0 and see where there are actually hits!
+//	for(unsigned j=0; j<events[0]->size(); j++)
+//		std::cout<< j << "\t" << events[0]->at(j).get_nHits()<<std::endl;
+	
+//	bcid = events[0]->at(10).getHits().back().get_row();
+//	std::cout<<"bcid: " << bcid <<std::endl;
+	unsigned a=0;
+//Try and look at the information in one of the hits
+	for(hitIt = events[0]->at(10).getHits().begin(); hitIt != events[0]->at(10).getHits().end(); ++hitIt){
+		std::cout<<a<<std::endl;
+		a++;
+		//bcid = (*hitIt).get_col();//events[0]->at(0).getHits().back().get_tot();
+		//std::cout<<"bcid: " <<bcid<<std::endl;
+	}
+
+/*
+	//Check functionality of pop_back
+	std::vector<int> testArr;
+	testArr.push_back(1);
+	testArr.push_back(2);
+	testArr.push_back(3);
+	
+	for(int i=0; i<testArr.size(); i++)
+	std::cout<<"Vector is: "<<testArr[i] << "\t";
+
+	testArr.pop_back();
+	std::cout<<""<<std::endl;
+
+	for(int i=0; i<testArr.size(); i++)
+	std::cout<<"Vector is: "<<testArr[i] << "\t";
+*/	
+	
+
+	
 }
