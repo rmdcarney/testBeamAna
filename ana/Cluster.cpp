47ae5fdc9fb0f361fb78b35be7d4b5a05702942e
carduelia <rmdcarney@gmail.com>
2014-12-03 13:31:53 -0800

    Added centre of charge functions

--- ana/Cluster.cpp
+++ ana/Cluster.cpp
@@ -9,7 +9,7 @@
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 #include <Cluster.h>
-#define DEBUG 1
+#define DEBUG 0
 
 Cluster::Cluster() {
 	size=0;
@@ -17,6 +17,9 @@ Cluster::Cluster() {
 	tot=0;
 	nDeltaRays=0;
 	lv1id=0;
+	totOverflow = false;
+	centreOfCharge.first = -1;
+	centreOfCharge.second = -1;
 	//Hit container - ready!
 	Hits hits;
 }
@@ -38,11 +41,78 @@ void Cluster::findToT(){
 				tot += i->get_tot();
 			} else {
 				tot = 0;
+				totOverflow = true;
 				break;
 			}
 		}
 	}
 }
+
+void Cluster::findCentreOfCharge(){
+
+	//Find mean position relative to cluster length
+	std::list<Hit>::iterator i;
+	centreOfCharge.first = -1;
+	centreOfCharge.second = -1;
+	unsigned sumx(0),sumy(0), toti(0);
+	unsigned bigx(0), smallx(1e5), bigy(0), smally(1e5);
+	double pcx(0.), pcy(0.);
+	unsigned row(0), col(0);
+	double range(0.), diffMean(0.);
+
+	//Check that the cluster tot has been calculated, if not, 
+	//calculate it
+	//Overflow, cannot calculate centre of charge
+	if(totOverflow == true)
+			return;
+	//Not yet been calculated
+	else if(tot == 0)
+		findToT();
+
+	//If it still can't calculate tot, quit
+	if(tot ==0)
+		return;
+	
+	//Iterate over hits
+	if(!hits.empty() && hits.size()>1){
+		for(i = hits.begin(); i!=hits.end(); ++i){
+			row = i->get_row();
+			col = i->get_col();
+			//Find the biggest and smallest
+			if(row > bigy)
+				bigy = row;
+			if(row < smally)
+				smally = row;
+			if(col > bigx)
+				bigx = col;
+			if(col < smallx)
+				smallx = col;
+
+			toti = i->get_tot();
+			sumx += (col*toti);
+			sumy += (row*toti);
+		}
+	}
+
+	//Now we want the percentage
+	pcx = (double)sumx;
+	pcx /= ((double)tot);
+	
+	range = bigx - smallx +1;
+	diffMean = pcx - smallx +1;
+	pcx = diffMean/range;
+
+	pcy = (double)sumy;
+	pcy /= ((double)tot);
+	
+	range = bigy - smally +1;
+	diffMean = pcy - smally +1;
+	pcy = diffMean/range;
+	
+	centreOfCharge.first = (pcx*100.);
+	centreOfCharge.second = (pcy*100.);
+}
+
 
 //Getters
 
@@ -68,7 +138,7 @@ unsigned Cluster::get_width(){
 }
 
 unsigned Cluster::get_totalToT(){
-	if(tot == 0)
+	if(tot == 0 && totOverflow == false)
 		findToT();
 	return tot;
 }
@@ -76,6 +146,13 @@ unsigned Cluster::get_totalToT(){
 unsigned Cluster::get_nDeltaRays(){
 	return nDeltaRays;
 }
+
+std::pair<double,double> Cluster::get_centreOfCharge(){
+	if(centreOfCharge.first == -1 && centreOfCharge.second == -1 && totOverflow == false)
+		findCentreOfCharge();
+	return centreOfCharge;
+}
+
 
 //Setters
 
